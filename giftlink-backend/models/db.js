07a0555ea620a14580887ba84bdb528aa564b144
sarthakchugh <sarthakchugh1997@gmail.com>
2024-10-27 03:53:13 -0400

    complete MainPage and Navbar

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -19,7 +19,7 @@ async function connectToDatabase() {
     await client.connect();
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    const dbInstance = client.db(giftdb);
+    dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
     return dbInstance;
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,4 +1,6 @@
 const connectToDatabase = require("../models/db");
+const express = require('express')
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
